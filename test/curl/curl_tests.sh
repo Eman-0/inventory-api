d063f156e82adca708f2d32ffad81d71f591761c
Emmanuel Aranda <emmanuelaranda27@gmail.com>
2021-04-06 22:26:49 -0500

    test

--- test/curl/curl_tests.sh
+++ test/curl/curl_tests.sh
@@ -22,8 +22,7 @@ fi
 #Get by ID not found
 curl --silent -X GET -H 'Content-Type: application/json' -d '{"id": 1}' https://10.100.201.3:12030/properties/id --insecure > actual.txt
 if grep -f ./test/curl/get_by_id_not_found.txt actual.txt; then
-    #pass
-    #echo "Pass"
+    echo "pass"
 else
     echo "Test Fail"
 fi
@@ -31,8 +30,8 @@ fi
 #Get All
 curl --silent -X GET https://10.100.201.3:12030/properties --insecure > actual.txt
 if grep -f ./test/curl/get_expected.txt actual.txt; then
-    #pass
-    #echo "Pass"
+    
+    echo "Pass"
 else
     echo "Test Fail"
 fi
@@ -40,8 +39,8 @@ fi
 #Delete no Auth
 curl --silent -X DELETE -d '{"id": 8}' https://10.100.201.3:12030/properties/id --insecure > actual.txt
 if grep -f ./test/curl/delete_by_id_no_auth.txt actual.txt; then
-    #pass
-    #echo "Pass"
+    
+    echo "Pass"
 else
     echo "Test Fail"
 fi
@@ -53,7 +52,7 @@ fi
 curl --silent -X POST -H 'Content-Type: application/json' -H 'Authorization: cs4783ftw!' -d '{"address":"4768 UTSA","city":"San Antonio","state":"Tx","zip":"78228"}' https://10.100.201.3:12030/properties --insecure > actual.txt
 if grep -f ./test/curl/post_expected.txt actual.txt; then
     #pass
-    #echo "Pass"
+    echo "Pass"
 else
     echo "Test Fail"
 fi
@@ -62,7 +61,7 @@ fi
 curl --silent -X POST -H 'Content-Type: application/json' -d '{"address":"4763 UTSA","city":"San Antonio","state":"Tx","zip":"78228"}' https://10.100.201.3:12030/properties --insecure > actual.txt
 if grep -f ./test/curl/post_no_auth.txt actual.txt; then
     #pass
-    #echo "Pass"
+    echo "Pass"
 else
     echo "Test Fail"
 fi
@@ -71,7 +70,7 @@ fi
 curl --silent -X PUT -H 'Content-Type: application/json' -d '{"id": 3,"address":"4763 UTSA","city":"San Antonio","state":"Tx","zip":"78228"}' https://10.100.201.3:12030/properties/id --insecure > actual.txt
 if grep -f ./test/curl/put_no_auth.txt actual.txt; then
     #pass
-    #echo "Pass"
+    echo "Pass"
 else
     echo "Test Fail"
 fi
@@ -80,7 +79,7 @@ fi
 curl --silent -X PUT -H 'Content-Type: application/json' -H 'Authorization: cs4783ftw!' -d '{"address":"4763 UTSA","city":"San Antonio","state":"Tx","zip":"78228","id":3}' https://10.100.201.3:12030/properties/id --insecure > actual.txt
 if grep -f ./test/curl/put_expected.txt actual.txt; then
     #pass
-    #echo "Pass"
+    echo "Pass"
 else
     echo "Test Fail"
 fi
