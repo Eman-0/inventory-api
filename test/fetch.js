400d2585c2b4a4d3f76acf0a34a9f7f0237a9a6d
Emmanuel Aranda <emmanuelaranda27@gmail.com>
2021-04-07 19:00:55 -0500

    Unit tests

---
+++ test/fetch.js
@@ -0,0 +1,49 @@
+let chai = require('chai');
+let property = require('../routes/Propertyfetch');
+let should = chai.should();
+let server = require('../server');
+let chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+//Our parent block
+describe('fetch.js', () => {
+    describe('Fetch Properties', () => {
+        it('it should get all the address properties in the database and return status code 200', (done) => {
+            chai.request(server)
+                .get('/properties')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+    });
+    describe('Fetch Property', () => {
+        it('it should fetch the info of a specified property given an id and return status code 200', (done) => {
+            chai.request(server)
+                .get('/properties/id')
+                .set('Content-Type', 'application/json')
+                .send("{\"id\": 3}")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('[{\"id\":3,\"address\":\"1225 Boardwalk\",\"city\":\"San Angles\",\"state\":\"CA\",\"zip\":\"9877\"}]');
+                    done();
+                });
+        });
+    });
+    describe('Fetch Non-Existing Property', () => {
+        it('it should attempt to get the info of a specified property given an id that does not exist, return an error message and status code 404', (done) => {
+            chai.request(server)
+                .get('/properties/id')
+                .set('Content-Type', 'application/json')
+                .send("{\"id\": 1}")
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.have.property('message').eql('Property not found')
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+    });
+});
