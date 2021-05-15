01f648003e750b7fbb4910e42ee212c1acbebb03
Emmanuel Aranda <emmanuelaranda27@gmail.com>
2021-05-15 12:55:47 -0500

    ci script

--- test/curl/curl_tests.sh
+++ test/curl/curl_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Hello Test
-curl --silent -X GET https://10.100.201.3:12030/hello --insecure > actual.txt
+curl --silent -X GET https://10.100.201.3:12031/hello --insecure > actual.txt
 if grep -f ./test/curl/hello_expected.txt actual.txt; then
     #pass
     echo "Pass"
@@ -11,7 +11,7 @@ fi
 
 
 #Get by ID
-curl --silent -X GET -H 'Content-Type: application/json' -d '{"id": 7}' https://10.100.201.3:12030/properties/id --insecure > actual.txt
+curl --silent -X GET -H 'Content-Type: application/json' -d '{"id": 7}' https://10.100.201.3:12031/properties/id --insecure > actual.txt
 if cmp -s ./test/curl/get_by_id.txt actual.txt; then
     echo "pass"
 else
@@ -20,7 +20,7 @@ fi
 
 
 #Get by ID not found
-curl --silent -X GET -H 'Content-Type: application/json' -d '{"id": 1}' https://10.100.201.3:12030/properties/id --insecure > actual.txt
+curl --silent -X GET -H 'Content-Type: application/json' -d '{"id": 1}' https://10.100.201.3:12031/properties/id --insecure > actual.txt
 if grep -f ./test/curl/get_by_id_not_found.txt actual.txt; then
     echo "pass"
 else
@@ -28,7 +28,7 @@ else
 fi
 
 #Get All
-curl --silent -X GET https://10.100.201.3:12030/properties --insecure > actual.txt
+curl --silent -X GET https://10.100.201.3:12031/properties --insecure > actual.txt
 if [ ! -s actual.txt ]; then
     
     echo "Test Fail"
@@ -37,7 +37,7 @@ else
 fi
 
 #Delete no Auth
-curl --silent -X DELETE -H "Content-Type: application/json" -d '{"id": 8}' https://10.100.201.3:12030/properties/id --insecure > actual.txt
+curl --silent -X DELETE -H "Content-Type: application/json" -d '{"id": 8}' https://10.100.201.3:12031/properties/id --insecure > actual.txt
 if grep -f ./test/curl/delete_by_id_no_auth.txt actual.txt; then
     
     echo "Pass"
@@ -46,7 +46,7 @@ else
 fi
 
 #Good Delete
-curl --silent -X DELETE -H "Content-Type: application/json" -H 'api_key: cs4783ftw!' -d '{"id": 1}' https://10.100.201.3:12030/properties/id --insecure > actual.txt
+curl --silent -X DELETE -H "Content-Type: application/json" -H 'api_key: cs4783ftw!' -d '{"id": 1}' https://10.100.201.3:12031/properties/id --insecure > actual.txt
 if grep -f ./test/curl/delete_by_id_not_found.txt actual.txt; then
     
     echo "Pass"
@@ -55,7 +55,7 @@ else
 fi
 
 #Good POST
-curl --silent -X POST -H 'Content-Type: application/json' -H 'api_key: cs4783ftw!' -d '{"address":"4768 UTSA","city":"San Antonio","state":"Tx","zip":"78228"}' https://10.100.201.3:12030/properties --insecure > actual.txt
+curl --silent -X POST -H 'Content-Type: application/json' -H 'api_key: cs4783ftw!' -d '{"address":"4768 UTSA","city":"San Antonio","state":"Tx","zip":"78228"}' https://10.100.201.3:12031/properties --insecure > actual.txt
 if grep -f ./test/curl/post_expected.txt actual.txt; then
     #pass
     echo "Pass"
@@ -64,7 +64,7 @@ else
 fi
 
 #Post no Auth
-curl --silent -X POST -H 'Content-Type: application/json' -d '{"address":"4763 UTSA","city":"San Antonio","state":"Tx","zip":"78228"}' https://10.100.201.3:12030/properties --insecure > actual.txt
+curl --silent -X POST -H 'Content-Type: application/json' -d '{"address":"4763 UTSA","city":"San Antonio","state":"Tx","zip":"78228"}' https://10.100.201.3:12031/properties --insecure > actual.txt
 if grep -f ./test/curl/post_no_auth.txt actual.txt; then
     #pass
     echo "Pass"
@@ -73,7 +73,7 @@ else
 fi
 
 #Put no auth
-curl --silent -X PUT -H 'Content-Type: application/json' -d '{"id": 3,"address":"4763 UTSA","city":"San Antonio","state":"Tx","zip":"78228"}' https://10.100.201.3:12030/properties/id --insecure > actual.txt
+curl --silent -X PUT -H 'Content-Type: application/json' -d '{"id": 3,"address":"4763 UTSA","city":"San Antonio","state":"Tx","zip":"78228"}' https://10.100.201.3:12031/properties/id --insecure > actual.txt
 if grep -f ./test/curl/put_no_auth.txt actual.txt; then
     #pass
     echo "Pass"
@@ -82,7 +82,7 @@ else
 fi
 
 #Good Put
-curl --silent -X PUT -H 'Content-Type: application/json' -H 'api_key: cs4783ftw!' -d '{"address":"4763 UTSA","city":"San Antonio","state":"Tx","zip":"78228","id":2}' https://10.100.201.3:12030/properties/id --insecure > actual.txt
+curl --silent -X PUT -H 'Content-Type: application/json' -H 'api_key: cs4783ftw!' -d '{"address":"4763 UTSA","city":"San Antonio","state":"Tx","zip":"78228","id":2}' https://10.100.201.3:12031/properties/id --insecure > actual.txt
 if grep -f ./test/curl/put_expected.txt actual.txt; then
     #pass
     echo "Pass"
