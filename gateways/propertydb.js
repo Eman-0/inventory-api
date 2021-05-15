43960c269f6ccbbc7ba10dae84e366386c992a94
Emmanuel Aranda <emmanuelaranda27@gmail.com>
2021-05-14 19:06:00 -0500

    gitlab variables

--- gateways/propertydb.js
+++ gateways/propertydb.js
@@ -5,17 +5,11 @@ const PropertyModel = require('../models/property-model');
 const utilities = require("../misc/utilities");
 const logger = utilities.getLogger();
 
-const sequelize = new Sequelize("cs4783_mio755", "root", "utsa", {
-	host: "mio755-db",
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+	host: process.env.DB_HOST,
 	dialect: "mysql"
 })
 
 const Property = PropertyModel(sequelize, Sequelize);
-
-// breaks as it doesn't wait for db to be ready
-// sequelize.sync()
-// .then(() => {
-// 	console.log('Synced models')
-// })
 
 module.exports = Property, sequelize;
