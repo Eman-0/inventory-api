95cecd0f60a7cffbec3e00d370a2fb7d8cbb31d8
Emmanuel Aranda <emmanuelaranda27@gmail.com>
2021-05-14 23:38:44 -0500

    kube

--- gateways/propertydb.js
+++ gateways/propertydb.js
@@ -1,12 +1,12 @@
 const { Sequelize, Model } = require('sequelize');
 const PropertyModel = require('../models/property-model');
-
+require('dotenv').config();
 
 const utilities = require("../misc/utilities");
 const logger = utilities.getLogger();
 
-const sequelize = new Sequelize("cs4783_mio755", "root", "utsa", {
-	host: "10.152.183.222",
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+	host: process.env.DB_HOST,
 	dialect: "mysql"
 })
 
